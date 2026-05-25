a45169aa8b4f4031be64bfd321f6a9b46e4b576c
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 16:20:34 +0300

    adjusting specific model which is loaded

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -6,7 +6,7 @@ class Backbone(nn.Module):
         super(Backbone, self).__init__()
 
         # Load SwinV2 Tiny pre-trained on ImageNet-22k wihtout classifier head
-        self.backbone = timm.create_model('swinv2_tiny_patch4_window8_256.ms_in22k', 
+        self.backbone = timm.create_model('swin_tiny_patch4_window7_224.ms_in22k', 
                                           pretrained=True, num_classes=0)
  
     def forward(self, image):
