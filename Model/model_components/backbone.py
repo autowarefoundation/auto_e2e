d996d8d9a4a549147b9bb1020f00910197c20db8
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 21:19:48 +0300

    making main model backbone trainable

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -8,14 +8,7 @@ class Backbone(nn.Module):
         # Load SwinV2 Tiny pre-trained on ImageNet-22k wihtout classifier head
         self.backbone = timm.create_model('swin_tiny_patch4_window7_224.ms_in22k', 
                                           pretrained=True, features_only=True)
-        
-        # Freeze all parameters in the backbone
-        for param in self.backbone.parameters():
-            param.requires_grad = False
-
-        # Set model to evaluation mode (crucial for freezing layers like Dropout/BatchNorm)
-        self.backbone.eval()
- 
+         
     def forward(self, image):
         features = self.backbone(image)
         return features   
