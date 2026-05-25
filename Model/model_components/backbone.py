3fa105c042da69465f934c5e4a5b6f21ad96ce16
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 17:36:14 +0300

    freezing pre-trained backbone model weights

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -7,7 +7,14 @@ class Backbone(nn.Module):
 
         # Load SwinV2 Tiny pre-trained on ImageNet-22k wihtout classifier head
         self.backbone = timm.create_model('swin_tiny_patch4_window7_224.ms_in22k', 
-                                          pretrained=True, num_classes=0)
+                                          pretrained=True, features_only=True)
+        
+        # Freeze all parameters in the backbone
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        # Set model to evaluation mode (crucial for freezing layers like Dropout/BatchNorm)
+        self.backbone.eval()
  
     def forward(self, image):
         features = self.backbone(image)
