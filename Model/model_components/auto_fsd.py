cc65e94ac49de77f17cc39d6ac57c21c1f0d7908
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 19:13:49 +0300

    adding FutureState prediction module

--- Model/model_components/auto_fsd.py
+++ Model/model_components/auto_fsd.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from .backbone import Backbone
 from .feature_fusion import FeatureFusion
 from .driving_policy import DrivingPolicy
+from .future_state import FutureState
 
 
 class AutoFSD(nn.Module):
@@ -14,15 +15,21 @@ class AutoFSD(nn.Module):
         # Multi-scale feature fusion
         self.FeatureFusion = FeatureFusion()
 
-        # Driving policy head
+        # Driving policy prediction
         self.DrivingPolicy = DrivingPolicy()
+
+        # Future visual state prediction
+        self.FutureState = FutureState()
    
 
     def forward(self, image, visual_history, egomotion_history):
+
         features = self.Backbone(image)
         fused_features = self.FeatureFusion(features)
 
         driving_policy, compressed_visual_feature_vector = \
             self.DrivingPolicy(fused_features, visual_history, egomotion_history)
         
-        return driving_policy, compressed_visual_feature_vector
+        future_visual_features = self.FutureState(fused_features)
+        
+        return driving_policy, compressed_visual_feature_vector, future_visual_features
