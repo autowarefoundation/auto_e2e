198ede96257adb8f50c6b3b7073007b0abed58cb
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-06-05 08:00:32 +0300

    passing in parameter to specify backbone

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -2,11 +2,11 @@ import torch.nn as nn
 from .backbones import build_backbone
 
 class Backbone(nn.Module):
-    def __init__(self):
+    def __init__(self, backbone="swinv2tiny"):
         super().__init__()
 
         # Pre-trained backbone (pluggable)
-        self.backbone = build_backbone()
+        self.backbone = build_backbone(backbone)
          
     def forward(self, image):
         features = self.backbone(image)
