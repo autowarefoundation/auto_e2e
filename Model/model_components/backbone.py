a25c9963671ccba00bb81cd941f32ca1de470776
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-06-05 07:59:08 +0300

    building backbone based on backbone registry

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -1,13 +1,12 @@
-import timm
 import torch.nn as nn
+from .backbones import build_backbone
 
 class Backbone(nn.Module):
     def __init__(self):
-        super(Backbone, self).__init__()
+        super().__init__()
 
-        # Load Swin V1 Tiny pre-trained on ImageNet-22k without classifier head
-        self.backbone = timm.create_model('swin_tiny_patch4_window7_224.ms_in22k', 
-                                          pretrained=True, features_only=True)
+        # Pre-trained backbone (pluggable)
+        self.backbone = build_backbone()
          
     def forward(self, image):
         features = self.backbone(image)
