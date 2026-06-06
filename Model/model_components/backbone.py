07d9fb7423dc9481be96751260991217c6cda17c
bharatwrrr <bharatwrrr@gmail.com>
2026-06-06 00:28:07 -0500

    add pretrained parameter to Backbone constructor for flexible model initialization
    Signed-off-by: bharatwrrr <bharatwrrr@gmail.com>

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -2,12 +2,12 @@ import torch.nn as nn
 from .backbones import build_backbone
 
 class Backbone(nn.Module):
-    def __init__(self, backbone="swin_v2_tiny"):
+    def __init__(self, backbone="swin_v2_tiny", pretrained: bool = True):
         super().__init__()
 
         # Pre-trained backbone (pluggable)
-        self.backbone = build_backbone(backbone)
+        self.backbone = build_backbone(backbone, pretrained=pretrained)
          
     def forward(self, image):
         features = self.backbone(image)
-        return features   
+        return features
