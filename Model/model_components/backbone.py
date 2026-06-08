e5b3529aacec3ae96623d8cfbc00f2dd22f49f5b
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-06-08 18:42:59 +0300

    calculating fused channel number for backbone and permuting features if they are from SwinV2 Tiny

--- Model/model_components/backbone.py
+++ Model/model_components/backbone.py
@@ -2,12 +2,22 @@ import torch.nn as nn
 from .backbones import build_backbone
 
 class Backbone(nn.Module):
-    def __init__(self, backbone="swin_v2_tiny", pretrained: bool = True):
+    def __init__(self, backbone="swin_v2_tiny", is_pretrained: bool = True):
         super().__init__()
 
         # Pre-trained backbone (pluggable)
-        self.backbone = build_backbone(backbone, pretrained=pretrained)
+        self.backbone = build_backbone(backbone, is_pretrained=is_pretrained)
+        self.backbone_name = backbone
          
     def forward(self, image):
         features = self.backbone(image)
-        return features   
+        backbone_channels = 0
+        for i in range(0, len(features)):
+            if(self.backbone_name == "swin_v2_tiny"):
+                features[i] = features[i].permute(0, 3, 1, 2)
+
+            _, C, _, _ = features[i].shape
+            print(features[i].shape)
+            backbone_channels += C
+            
+        return features, backbone_channels
