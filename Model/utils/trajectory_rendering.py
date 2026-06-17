472da8442e385674d4ce6531147a03959616c411
Arseni10Lk <arseniy230606@gmail.com>
2026-06-17 16:06:08 +0200

    Added trajectory visualization
    Signed-off-by: Arseni10Lk <arseniy230606@gmail.com>

--- Model/utils/trajectory_rendering.py
+++ Model/utils/trajectory_rendering.py
@@ -1,6 +1,9 @@
 import torch
-import numpy as np
 from PIL import Image, ImageDraw
+import math
+
+_DT = 0.1  # 10 Hz
+_FUTURE_TIMESTEPS = 64
 
 class Visualization:
 
@@ -25,8 +28,46 @@ class Visualization:
             A new PIL Image with the trajectory drawn on it.
         """
 
+        # change the trajectory format
+        action_sequence = torch.reshape(action_sequence, (_FUTURE_TIMESTEPS, 2))
+
         # 1. Convert trajectory to [x y] in meters
+
+        trajectory_m = torch.zeros((_FUTURE_TIMESTEPS + 1, 2))
+        trajectory_m[0, :] = 0
+
+        # 1.1 velocity is needed for integration
+        v = float(current_speed)
+
+        # 1.2 Yaw angle is needed to derive 2D acceleration
+        yaw = 0.0
+
+        for i in range(_FUTURE_TIMESTEPS):
+            accel = action_sequence[i, 0].item()
+            curv = action_sequence[i, 1].item()
+
+            v = v + (accel * _DT)
+            yaw = yaw + (v * curv * _DT)
+
+            # The format is [X Y]. Sign convention for yaw is + = CCW
+            trajectory_m[i + 1, 0] = trajectory_m[i, 0] - (v * math.sin(yaw) * _DT)
+            trajectory_m[i + 1, 1] = trajectory_m[i, 1] + (v * math.cos(yaw) * _DT)
 
         # 2. Convert meters to pixels
 
+        W, H = map_image.size
+
+        trajectory_px = torch.zeros_like(trajectory_m)
+        trajectory_px[:, 0] = ((trajectory_m[:, 0] + radius_m) / (2 * radius_m)) * W
+        trajectory_px[:, 1] = ((radius_m - trajectory_m[:, 1]) / (2 * radius_m)) * H
+
         # 3. Overlay the trajectory onto the map tile
+
+        pixel_points = [(x.item(), y.item()) for x, y in trajectory_px]
+
+        map_with_trajectory = map_image.copy()
+
+        draw = ImageDraw.Draw(map_with_trajectory)
+        draw.line(pixel_points, fill="#33FF33", width=3)
+
+        return map_with_trajectory
