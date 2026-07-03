c8887490464f542f3f6012e58ef5f0b64ce6861c
riita10069 <ryota10069.tech5.jizi@gmail.com>
2026-07-03 19:50:17 +0900

    feat(l2d): export MAP_VIEW_NAME and load_map_frame
    Signed-off-by: riita10069 <ryota10069.tech5.jizi@gmail.com>

--- Model/data_parsing/l2d/__init__.py
+++ Model/data_parsing/l2d/__init__.py
@@ -1,12 +1,21 @@
-from .camera import CAMERA_NAMES, NUM_VIEWS, load_camera_frames, make_camera_params_placeholder
+from .camera import (
+    CAMERA_NAMES,
+    MAP_VIEW_NAME,
+    NUM_VIEWS,
+    load_camera_frames,
+    load_map_frame,
+    make_camera_params_placeholder,
+)
 from .dataset import L2DDataset
 from .egomotion import EGOMOTION_DIM, extract_egomotion
 
 __all__ = [
     "L2DDataset",
     "load_camera_frames",
+    "load_map_frame",
     "make_camera_params_placeholder",
     "CAMERA_NAMES",
+    "MAP_VIEW_NAME",
     "extract_egomotion",
     "NUM_VIEWS",
     "EGOMOTION_DIM",
